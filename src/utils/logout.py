fc56a97b49f9370763efb7588a8463230644ba57
AlexeyGrushev <yablokov2004@mail.ru>
2024-05-21 20:44:21 +0300

    Deleting the last lesson result when leaving your profile

--- src/utils/logout.py
+++ src/utils/logout.py
@@ -7,6 +7,10 @@ from src.utils.right_rerun import right_rerun
 def logout(st: streamlit, cookie_manager: CookieController):
     try:
         cookie_manager.remove("user_access")
+        try:
+            cookie_manager.remove("last_lesson_result")
+        except Exception:
+            pass
         right_rerun(st, cookie_manager)
     except Exception:
         pass
